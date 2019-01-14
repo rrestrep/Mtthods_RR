512c8c13ed6d91ce69c14e43f82d7decfc1bec2f
R Res <clonnes@hotmail.com>
2019-01-14 14:30:39 -0500

    Add project files.

---
+++ Mtthods_RR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mtthods_RR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
